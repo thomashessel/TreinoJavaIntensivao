837d8f6c034059f29548fadb6c06126c02f161f0
thomashessel <thomas.silva482@gmail.com>
2023-05-09 21:34:58 -0300

    implementado o belongin com Embeddable e chave composta

---
+++ src/main/java/com/hessel/dslist/entities/BelongingPk.java
@@ -0,0 +1,62 @@
+package com.hessel.dslist.entities;
+
+import java.util.Objects;
+
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Embeddable
+public class BelongingPk {
+	
+	@ManyToOne
+	@JoinColumn(name = "gameId")
+	private Game game;
+	
+	@ManyToOne
+	@JoinColumn(name = "gameLsId")	
+	private GameList gameList;
+	
+	public BelongingPk() {}
+
+	public BelongingPk(Game game, GameList gameList) {
+		super();
+		this.game = game;
+		this.gameList = gameList;
+	}
+
+	public Game getGame() {
+		return game;
+	}
+
+	public void setGame(Game game) {
+		this.game = game;
+	}
+
+	public GameList getGameList() {
+		return gameList;
+	}
+
+	public void setGameList(GameList gameList) {
+		this.gameList = gameList;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(game, gameList);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BelongingPk other = (BelongingPk) obj;
+		return Objects.equals(game, other.game) && Objects.equals(gameList, other.gameList);
+	}
+	
+}
--- src/main/java/com/hessel/dslist/entities/GameList.java
+++ src/main/java/com/hessel/dslist/entities/GameList.java
@@ -1,5 +1,7 @@
 package com.hessel.dslist.entities;
 
+import java.util.Objects;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -38,6 +40,23 @@ public class GameList {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GameList other = (GameList) obj;
+		return Objects.equals(id, other.id);
 	}
 	
 }
